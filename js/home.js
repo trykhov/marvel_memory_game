2b55cc97d8dd1611ae817ef37c91e64eea8aceca
Try <khovct@gmail.com>
2019-02-23 11:40:56 -0800

    making the infinity stones light up

--- js/home.js
+++ js/home.js
@@ -1,10 +1,34 @@
 const infinityStones = ["red", "blue", "green", "yellow", "purple", "orange"];
-var hoverColor = randomColor(); // picks a color when button is hovered over
 
 $(".btn").hover(function() {
-  $("#" + this.id).css("background-color", randomColor());},
+  var stoneColor = randomColor();
+  $("#" + this.id).css("background-color", stoneColor);
+  switch(stoneColor) {
+    case "red":
+      $("#reality").css("background-color", stoneColor);
+      break;
+    case "blue":
+      $("#space").css("background-color", stoneColor);
+      break;
+    case "green":
+      $("#time").css("background-color", stoneColor);
+      break;
+    case "yellow":
+      $("#mind").css("background-color", stoneColor);
+      break;
+    case "purple":
+      $("#power").css("background-color", stoneColor);
+      break;
+    case "orange":
+      $("#soul").css("background-color", stoneColor);
+      break;
+    default:
+
+    }
+  },
   function() {
     $("#" + this.id).css("background-color", "gray");
+    $(".stones").css("background-color", "gray");
   });
 
 // pick a random color from the infinityStones list
