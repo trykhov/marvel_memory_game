7036d1f01dca87c27a5b8b1269cd9a2ee8b4b3e9
Try <khovct@gmail.com>
2019-02-23 23:20:38 -0800

    created theshuffle function, got the characters to flip and flip back

--- js/mechanics.js
+++ js/mechanics.js
@@ -1,4 +1,35 @@
-// flips the choice
+var phase = $("header h1").text();
+switch(phase) {
+  case "Phase I":
+    var totalCharacters = 6 * 2; // 6 characters in Phase I
+    break;
+  case "Phase II":
+    var totalCharacters = 12 * 2; // 12 total characters in Phase II
+    break;
+  case "Phase III":
+    var totalCharacters = 16 * 2; // 32 total characters in Phase III
+    break;
+};
+
+var characterID = ""; // this is so we can implement "this" in setTimeout
+
 $(".character").click(function() {
-  $(this).toggleClass("flip");
-})
+  characterID = "#" + this.id;
+  $(characterID).toggleClass("flip");
+  setTimeout(function() {
+    $(characterID).toggleClass("flip");
+  }, 3000);
+});
+
+
+function shuffle(characterList) {
+  var placeHolder = "";
+  var randomIndex = 0;
+  for(let i = 0; i < characterList.length; i++) {
+    randomIndex = Math.floor(Math.random() * characterList.length);
+    placeHolder = characterList[i];
+    characterList[i] = characterList[randomIndex];
+    characterList[randomIndex] = placeHolder;
+  };
+  return characterList;
+}
