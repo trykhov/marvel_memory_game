bfa2f92ea2bcc37aadfa4b43895d3848ae755456
Try <khovct@gmail.com>
2019-02-24 18:03:32 -0800

    managed to get the images to be randomly assorted

--- js/mechanics.js
+++ js/mechanics.js
@@ -1,22 +1,38 @@
 var phase = $("header h1").text();
-var choices = [] // this will have a max of two choices
+var choices = []; // this will have a max of two choices
+var phaseI = ["<img src='images/black_widow.jpg' alt='bw'>", "<img src='images/black_widow.jpg' alt='bw'>",
+              "<img src='images/captain_america.jpg' alt='capt'>", "<img src='images/captain_america.jpg' alt='capt'>",
+              "<img src='images/hawkeye.jpg' alt='hawk'>", "<img src='images/hawkeye.jpg' alt='hawk'>",
+              "<img src='images/hulk.jpeg' alt='hulk'>", "<img src='images/hulk.jpeg' alt='hulk'>",
+              "<img src='images/iron_man.jpg' alt='tony'>", "<img src='images/iron_man.jpg' alt='tony'>",
+              "<img src='images/thor.jpg' alt='thor'>","<img src='images/thor.jpg' alt='thor'>"];
+// var phaseII = phaseI + [];
+
+
+
 switch(phase) {
   case "Phase I":
     var totalCharacters = 6 * 2; // 6 characters in Phase I
+    var timeLimit = 3000; // 3000 milliseconds = 3 seconds
+    var phaseLevel = shuffle(phaseI);
     break;
   case "Phase II":
     var totalCharacters = 12 * 2; // 12 total characters in Phase II
+    var timeLimit = 2000; // 2000 milliseconds = 2 seconds;
     break;
   case "Phase III":
     var totalCharacters = 16 * 2; // 32 total characters in Phase III
+    var timeLimit = 1000; // 1000 milliseconds = 1 second
     break;
 };
 
-// gets the id of all the character elements
-var idList = [];
+
+// this inserts the randomly assorted images into the elements
 for(let i = 1; i <= totalCharacters; i++) {
-  idList.push("#s" + i);
+  $("#s" + i + " .back").html(phaseLevel[i - 1]);
 }
+
+
 
 
 $(".character").click(function() {
@@ -45,16 +61,14 @@ function checkID(characterList) {
     if(characterList[0] === characterList[1]) {
       $("#" + characterList[0]).toggleClass("flip");
       $("#" + characterList[1]).toggleClass("flip");
-      console.log("correct");
       choices = [];
     } else {
       $("#" + characterList[0]).toggleClass("flip");
       $("#" + characterList[1]).toggleClass("flip");
-      console.log("wrong");
       setTimeout(function() {
         $("#" + characterList[0]).toggleClass("flip");
         $("#" + characterList[1]).toggleClass("flip");
-      }, 3000);
+      }, timeLimit);
       choices = []
     }
   } else if(characterList.length > 2) {
